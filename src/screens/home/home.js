bfe4d49dd10fdaec7917cc601da09dc226432f0f
ManarMohamed1 <Manar.mohamed17.88@gmail.com>
2021-07-13 14:53:22 +0200

    home free data

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,56 +1,5 @@
-/**
- * @description      :
- * @author           : Nonaa
- * @group            :
- * @created          : 07/07/2021 - 00:06:26
- *
- * MODIFICATION LOG
- * - Version         : 1.0.0
- * - Date            : 07/07/2021
- * - Author          : Nonaa
- * - Modification    :
- * */
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { CallInAction, Rating } from "../../components";
-import api from "../../services/api";
-import { Banner, HowItWorks, CategoryItems, DownloadApplication, Categories } from "./index";
-
-import { updateSpinnerStatus } from "../../redux/actions";
+import React from "react";
 
 export const Home = () => {
-  const dispatch = useDispatch();
-  const [homeData, setHomeData] = useState(null);
-
-  // const [{ loading: homeLoading }, fetchHomeData] = api.useAxios({
-  //   url: "/landing-page",
-  //   method: "get",
-  // });
-
-  // console.log("homeLoading", homeLoading);
-
-  // useEffect(() => {
-  //   fetchHomeData().then((res) => {
-  //     setHomeData(res.data);
-  //   });
-  //   return () => {
-  //     dispatch(updateSpinnerStatus({ homeLoading: false }));
-  //   };
-  // }, []);
-
-  // useEffect(() => {
-  //   dispatch(updateSpinnerStatus({ homeLoading }));
-  // }, [homeLoading]);
-
-  return (
-    <div style={{ minHeight: "60vh" }}>
-      {/* <Banner />
-      <HowItWorks />
-      <Categories />
-      <CategoryItems />
-      <DownloadApplication />
-      <CallInAction />
-      <Rating /> */}
-    </div>
-  );
+  return <div style={{ minHeight: "60vh" }}>Home</div>;
 };
--- src/screens/home/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./banner";
-export * from "./howItWorks";
-export * from "./featuredItems";
-export * from "./trendingItems";
-export * from "./categoryItems";
-export * from "./downloadApplication";
-export * from "./categories";
